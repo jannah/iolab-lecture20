7a3fa5791bf49213b6114cb7ebbe207e1de2c021
jannah <hassan.jannah@gmail.com>
2013-11-25 13:58:24 -0800

    init

--- app.py
+++ app.py
@@ -23,10 +23,14 @@ def index():
     except IOError:
         initFile()
 
+    
+    s = "Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/532.9 (KHTML, like Gecko) Chrome/5.0.307.11 Safari/532.9"
+    os = httpagentparser.detect(s)
+    print (os)
     num = 30
 
     #redis.set('visitors', num)
-    return render_template('index.html', number=num, win_num=win_count, mac_num=mac_count )
+    return render_template('index.html', number=num, win_num=win_count, mac_num=mac_count, user_os=os )
 def process():
     print ('reading file')
     global win_count
